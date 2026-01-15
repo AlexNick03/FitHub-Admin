80241db73d192cd428cc9896862c7b0f9cd0b1d3
iustinflorian <iustingaita.f@gmail.com>
2026-01-15 20:09:57 +0200

    Configurare structurala si conexiune baza de date
    - Initializare proiect .NET 8 Web API
    - Configurare Entity Framework Core cu SQLite
    - Setup Swagger pentru testare API
    - Configurare Dependency Injection

---
+++ FitHubAdmin/FitHubAdmin.API/Program.cs
@@ -0,0 +1,34 @@
+﻿using FitHubAdmin.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=fithub.db"));
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated(); 
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "FitHub Admin API v1");
+    c.RoutePrefix = "swagger";
+});
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
